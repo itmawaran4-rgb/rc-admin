2588e6162b8a74005059d1ff3c76f32ef6fc2189
itmawaran4-rgb <itmawaran4@gmail.com>
2026-06-27 14:53:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,11 @@
 const CACHE = 'rcw-v1';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-      )
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
 });
-
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
